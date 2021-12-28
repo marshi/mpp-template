6e382f3ac9ba64e01b33309d337be21b1f57d1c5
marshi <mashi3648@gmail.como>
2021-12-28 16:47:58 +0900

    common

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,26 @@
+initDeps(project)
+
 buildscript {
-    repositories {
-        gradlePluginPortal()
-        jcenter()
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
-        classpath("com.android.tools.build:gradle:7.0.2")
-    }
+  repositories {
+    gradlePluginPortal()
+    jcenter()
+    google()
+    mavenCentral()
+  }
+  dependencies {
+//    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
+    classpath(Deps.JetBrains.Kotlin.gradlePlugin)
+    classpath(Deps.Android.Tools.Build.gradlePlugin)
+  }
 }
 
 group = "dev.marshi"
 version = "1.0"
 
 allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
+  repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+  }
 }
